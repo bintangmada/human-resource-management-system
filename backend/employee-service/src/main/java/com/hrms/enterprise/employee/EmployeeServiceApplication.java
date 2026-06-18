1653dd00278a55732fbb8712a04ac6084a8482fa
Bintang Mada <44053984+bintangmada@users.noreply.github.com>
2026-06-19 05:45:00 +0700

    refactor: rename hris package and configurations to hrms

--- backend/employee-service/src/main/java/com/hrms/enterprise/employee/EmployeeServiceApplication.java
+++ backend/employee-service/src/main/java/com/hrms/enterprise/employee/EmployeeServiceApplication.java
@@ -1,4 +1,4 @@
-package com.hris.enterprise.employee;
+package com.hrms.enterprise.employee;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
